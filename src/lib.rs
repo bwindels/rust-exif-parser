1b5d85bbe0ba1becabd8bd4d5bde7ccc16ad9c1c
Bruno Windels <bruno.windels@gmail.com>
2016-09-18 13:39:06 +0200

    rename BufferStream to Cursor

--- src/lib.rs
+++ src/lib.rs
@@ -77,15 +77,15 @@ fn to_be<T: ByteSwappable>(n: T) -> T {
     n.swap_bytes()
 }
 
-pub struct BufferStream<'a> {
+pub struct Cursor<'a> {
     data: &'a [u8],
     offset: usize,
     endianness: Endianness
 }
 
-impl<'a> BufferStream<'a> {
-    pub fn new(data: &'a [u8], init_endian: Endianness) -> BufferStream<'a> {
-        BufferStream {
+impl<'a> Cursor<'a> {
+    pub fn new(data: &'a [u8], init_endian: Endianness) -> Cursor<'a> {
+        Cursor {
             data: data,
             offset: 0,
             endianness: init_endian
@@ -145,20 +145,20 @@ impl<'a> BufferStream<'a> {
         }
     }
 
-    pub fn branch(&self, length: usize) -> Option<BufferStream<'a>> {
+    pub fn branch(&self, length: usize) -> Option<Cursor<'a>> {
         if self.len() >= length {
             let data_subset = &self.data[self.offset .. self.offset + length];
-            Some(BufferStream::new(data_subset, self.endianness))
+            Some(Cursor::new(data_subset, self.endianness))
         } else {
             None
         }
     }
     
-    pub fn skip(&self, length: usize) -> Option<BufferStream<'a>> {
+    pub fn skip(&self, length: usize) -> Option<Cursor<'a>> {
     	if self.len() >= length {
     		let new_offset = self.offset + length;
     		let slice = &self.data[new_offset ..];
-    		Some(BufferStream::new(slice, self.endianness))
+    		Some(Cursor::new(slice, self.endianness))
     	}
     	else {
     		None
@@ -172,13 +172,13 @@ mod tests {
 
     #[test]
     fn test_len() {
-        let stream = ::BufferStream::new(&DATA, ::Endianness::Big);
+        let stream = ::Cursor::new(&DATA, ::Endianness::Big);
         assert_eq!(stream.len(), 4);
     }
 
     #[test]
     fn test_read_u8() {
-        let mut stream = ::BufferStream::new(&DATA, ::Endianness::Big);
+        let mut stream = ::Cursor::new(&DATA, ::Endianness::Big);
         assert_eq!(stream.read_num::<u8>(), Some(0xDE));
         assert_eq!(stream.read_num::<u8>(), Some(0xAD));
         assert_eq!(stream.read_num::<u8>(), Some(0xCA));
@@ -189,7 +189,7 @@ mod tests {
     #[test]
     fn test_read_u16_big_endian() {
         let data = &[0xFD, 0xE8];
-        let mut stream = ::BufferStream::new(data, ::Endianness::Big);
+        let mut stream = ::Cursor::new(data, ::Endianness::Big);
         assert_eq!(stream.read_num::<u16>(), Some(65000));
         assert_eq!(stream.read_num::<u16>(), None);
     }
@@ -197,14 +197,14 @@ mod tests {
     #[test]
     fn test_read_u16_little_endian() {
         let data = &[0xE8, 0xFD];
-        let mut stream = ::BufferStream::new(data, ::Endianness::Little);
+        let mut stream = ::Cursor::new(data, ::Endianness::Little);
         assert_eq!(stream.read_num::<u16>(), Some(65000));
         assert_eq!(stream.read_num::<u16>(), None);
     }
 
     #[test]
     fn test_read_u16_switch_endianness() {
-        let mut stream = ::BufferStream::new(&DATA, ::Endianness::Big);
+        let mut stream = ::Cursor::new(&DATA, ::Endianness::Big);
         assert_eq!(stream.read_num::<u16>(), Some(0xDEAD));
         stream.set_endianness(::Endianness::Little);
         assert_eq!(stream.read_num::<u16>(), Some(0xFECA));
@@ -212,14 +212,14 @@ mod tests {
 
     #[test]
     fn test_read_u32_big_endian() {
-        let mut stream = ::BufferStream::new(&DATA, ::Endianness::Big);
+        let mut stream = ::Cursor::new(&DATA, ::Endianness::Big);
         assert_eq!(stream.read_num::<u32>(), Some(0xDEADCAFE));
         assert_eq!(stream.read_num::<u32>(), None);
     }
 
     #[test]
     fn test_read_u32_little_endian() {
-        let mut stream = ::BufferStream::new(&DATA, ::Endianness::Little);
+        let mut stream = ::Cursor::new(&DATA, ::Endianness::Little);
         assert_eq!(stream.read_num::<u32>(), Some(0xFECAADDE));
         assert_eq!(stream.read_num::<u32>(), None);
     }
@@ -227,7 +227,7 @@ mod tests {
     #[test]
     fn test_read_f32_little_endian() {
         let data = &[0xD8, 0xF, 0x49, 0x40];
-        let mut stream = ::BufferStream::new(data, ::Endianness::Little);
+        let mut stream = ::Cursor::new(data, ::Endianness::Little);
         assert_eq!(stream.read_num::<f32>(), Some(3.141592));
         assert_eq!(stream.read_num::<f32>(), None);
     }
@@ -235,7 +235,7 @@ mod tests {
     #[test]
     fn test_read_f32_big_endian() {
         let data = &[0x40, 0x49, 0xF, 0xD8];
-        let mut stream = ::BufferStream::new(data, ::Endianness::Big);
+        let mut stream = ::Cursor::new(data, ::Endianness::Big);
         assert_eq!(stream.read_num::<f32>(), Some(3.141592));
         assert_eq!(stream.read_num::<f32>(), None);
     }
@@ -243,7 +243,7 @@ mod tests {
     #[test]
     fn test_read_f64_little_endian() {
         let data = &[0xEA, 0x2E, 0x44, 0x54, 0xFB, 0x21, 0x9, 0x40];
-        let mut stream = ::BufferStream::new(data, ::Endianness::Little);
+        let mut stream = ::Cursor::new(data, ::Endianness::Little);
         assert_eq!(stream.read_num::<f64>(), Some(3.14159265359));
         assert_eq!(stream.read_num::<f64>(), None);
     }
@@ -251,7 +251,7 @@ mod tests {
     #[test]
     fn test_read_f64_big_endian() {
         let data = &[0x40, 0x9, 0x21, 0xFB, 0x54, 0x44, 0x2E, 0xEA];
-        let mut stream = ::BufferStream::new(data, ::Endianness::Big);
+        let mut stream = ::Cursor::new(data, ::Endianness::Big);
         assert_eq!(stream.read_num::<f64>(), Some(3.14159265359));
         assert_eq!(stream.read_num::<f64>(), None);
     }
@@ -259,14 +259,14 @@ mod tests {
     #[test]
     fn test_read_str() {
         let data = &[0x68, 0x65, 0x6C, 0x6C, 0x6F];
-        let mut stream = ::BufferStream::new(data, ::Endianness::Little);
+        let mut stream = ::Cursor::new(data, ::Endianness::Little);
         assert_eq!(stream.read_str(5), Some("hello"));
         assert_eq!(stream.read_str(1), None);
     }
 
     #[test]
     fn test_branch() {
-        let mut stream = ::BufferStream::new(DATA, ::Endianness::Big);
+        let mut stream = ::Cursor::new(DATA, ::Endianness::Big);
         let mut branched_stream = stream.branch(2).unwrap();
         let result = Some(0xDEAD);
 
@@ -279,7 +279,7 @@ mod tests {
 
     #[test]
     fn test_branch_length_check() {
-        let stream = ::BufferStream::new(DATA, ::Endianness::Big);
+        let stream = ::Cursor::new(DATA, ::Endianness::Big);
 
         assert!(stream.branch(4).is_some());
         assert!(stream.branch(5).is_none());
@@ -287,7 +287,7 @@ mod tests {
 
     #[test]
     fn test_skip() {
-        let mut stream = ::BufferStream::new(&DATA, ::Endianness::Big);
+        let mut stream = ::Cursor::new(&DATA, ::Endianness::Big);
         stream = stream.skip(2).unwrap();
         assert_eq!(stream.read_num::<u16>(), Some(0xCAFE));
         assert_eq!(stream.read_num::<u16>(), None);
