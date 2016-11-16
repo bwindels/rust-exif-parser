1ef977300ad3c766d96a51e70d459d172ac404ff
Bruno Windels <bruno.windels@gmail.com>
2016-11-16 10:10:48 +0100

    implement exif subvalue iterator
    since an exif value can be something like an integer fraction
    I can't use the ByteSwappable trait straight away here, so
    I built the ExifValueReader trait around it, for the special cases

--- src/exif.rs
+++ src/exif.rs
@@ -3,11 +3,71 @@ use ::cursor::{Cursor, Endianness, ByteSwappable};
 use ::error::{ParseError, ParseResult};
 use std::marker::{Sized, PhantomData};
 
+trait ExifValueReader {
+  fn read_exif_value(cursor: &mut Cursor) -> ParseResult<Self> where Self: Sized + Copy;
+}
+
+impl ExifValueReader for (u32, u32) {
+  fn read_exif_value(cursor: &mut Cursor) -> ParseResult<Self> {
+    let a : u32 = try!(cursor.read_num_or_fail());
+    let b : u32 = try!(cursor.read_num_or_fail());
+    Ok((a, b))
+  }
+}
+
+impl ExifValueReader for (i32, i32) {
+  fn read_exif_value(cursor: &mut Cursor) -> ParseResult<Self> {
+    let a : i32 = try!(cursor.read_num_or_fail());
+    let b : i32 = try!(cursor.read_num_or_fail());
+    Ok((a, b))
+  }
+}
+
+impl<T> ExifValueReader for T where T: ByteSwappable {
+  fn read_exif_value(cursor: &mut Cursor) -> ParseResult<Self> {
+    cursor.read_num_or_fail()
+  }
+}
+
 pub struct ValueIterator<'a, T> {
   value_cursor: Cursor<'a>,
   len: u32,
   i: u32,
   phantom_data: PhantomData<T>
+}
+
+impl<'a, T: ExifValueReader + Copy + Sized> ValueIterator<'a, T> {
+  fn new(cursor: Cursor<'a>, len: u32) -> ValueIterator<'a, T> {
+    ValueIterator {
+      value_cursor: cursor,
+      len: len,
+      i: 0,
+      phantom_data: PhantomData
+    }
+  }
+}
+
+impl<'a, T: ExifValueReader + Copy + Sized> Iterator for ValueIterator<'a, T> {
+
+  type Item = T;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    let remaining = self.len - self.i;
+
+    if remaining == 0 {
+      return None;
+    }
+
+    self.i = remaining;
+
+    let value = T::read_exif_value(&mut self.value_cursor);
+    return Some(value.unwrap());
+  }
+
+  fn size_hint(&self) -> (usize, Option<usize>) {
+    let remaining = (self.len - self.i) as usize;
+    (remaining, Some(remaining))
+  }
 }
 
 pub enum ExifVariant<'a> {
