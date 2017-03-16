f784e1e13fb74a1ad8d79c5aa2cea84744bea609
Bruno Windels <bruno.windels@gmail.com>
2017-03-16 21:03:51 +0100

    cleanup whitespace

--- src/exif.rs
+++ src/exif.rs
@@ -210,33 +210,33 @@ pub fn read_exif_header<'a>(app1_cursor: &mut Cursor<'a>) -> ParseResult<Cursor<
 	let header = app1_cursor.read_bytes_or_fail(6)?;
 
 	if header != b"Exif\0\0" {
-   let h = header;
-   let header_array = [h[0], h[1], h[2], h[3], h[4], h[5]];
-   return Err(ParseError::InvalidExifHeader{ header: header_array });
- }
+    let h = header;
+    let header_array = [h[0], h[1], h[2], h[3], h[4], h[5]];
+    return Err(ParseError::InvalidExifHeader{ header: header_array });
+  }
 
- let mut tiff_marker = app1_cursor.clone();
- let tiff_header : u16 = app1_cursor.read_num_or_fail()?;
+  let mut tiff_marker = app1_cursor.clone();
+  let tiff_header : u16 = app1_cursor.read_num_or_fail()?;
 
- if tiff_header == 0x4949 {
-  app1_cursor.set_endianness(Endianness::Little);
-}
-else if tiff_header == 0x4D4D {
-  app1_cursor.set_endianness(Endianness::Big);
-}
-else {
-  return Err(ParseError::InvalidTiffHeader{ header: tiff_header });
-}
+  if tiff_header == 0x4949 {
+    app1_cursor.set_endianness(Endianness::Little);
+  }
+  else if tiff_header == 0x4D4D {
+    app1_cursor.set_endianness(Endianness::Big);
+  }
+  else {
+    return Err(ParseError::InvalidTiffHeader{ header: tiff_header });
+  }
 
-let tiff_data_marker : u16 = app1_cursor.read_num_or_fail()?;
+  let tiff_data_marker : u16 = app1_cursor.read_num_or_fail()?;
 
-if tiff_data_marker != 0x002A {
-  return Err(ParseError::InvalidTiffData{ data: tiff_data_marker });
-}
+  if tiff_data_marker != 0x002A {
+    return Err(ParseError::InvalidTiffData{ data: tiff_data_marker });
+  }
 
-tiff_marker.set_endianness(app1_cursor.endianness());
+  tiff_marker.set_endianness(app1_cursor.endianness());
 
-return Ok(tiff_marker);
+  return Ok(tiff_marker);
 }
 
 #[cfg(test)]
